f5561f98074d27b58d31608412c6fb57dc2f15aa
Omid <omid.alemi@gmail.com>
2016-09-19 01:05:49 -0700

    first commit - working with basic heatmap/google map

---
+++ README.md
@@ -0,0 +1,11 @@
+# Ja/Viz
+Visualization of Google location history data
+
+## Credits
+Created by [Omid Alemi](http://omid.al)
+
+Built using the [React-Webpack-Babel](https://github.com/alicoding/react-webpack-babel) starter code
+
+
+## License
+This code is available under the [MIT license](http://opensource.org/licenses/MIT).
---
+++ src/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render } from 'react-dom';
+import App from './app.jsx';
+import $ from 'jquery';
+import '../assets/spinner.css';
+
+import lochistoryjson from "file!./data/LocationHistory.json"
+
+ $.ajax({
+      url: lochistoryjson,
+      dataType: 'json',
+      cache: true,
+      success: function(lochistory) {
+          console.log(lochistory);
+
+        render(<App lochistory={lochistory.locations}/>, document.querySelector("#app"));
+      }
+ });
+
+
